cc6ec46b3b7ddfd7a8445c50e4a1ecbb583b3d16
Caillou <Boucher.Pierre@hotmail.fr>
2018-10-11 15:53:27 +0200

    Tentative de réinitialisation de CV

---
+++ nouveauCv/assets/js/cvNum.js
@@ -0,0 +1,8 @@
+function displayShow(div) {
+    Presentation.style.display = "none";
+    Competences.style.display = "none";
+    Formation.style.display = "none";
+    Experiences.style.display = "none";
+    Contact.style.display = "none";
+    document.getElementById(div).style.display = "block";
+}
