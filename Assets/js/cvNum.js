43c26af8ba65d86d9969587c1f54bfcaf3e0ad02
PierreB <floods@hotmail.fr>
2019-01-15 15:13:53 +0100

    Formatage fonction javascript en jquery

--- Assets/js/cvNum.js
+++ Assets/js/cvNum.js
@@ -1,23 +1,11 @@
-//$('document').ready(function(){
-
-	function displayShow(div) {
-	    document.getElementById('caillouPresentation').style.display = "none";
-	    document.getElementById('caillouCompetences').style.display = "none";
-	    document.getElementById('caillouFormation').style.display = "none";
-	    document.getElementById('caillouExperiences').style.display = "none";
-	    document.getElementById('caillouContact').style.display = "none";
-	    document.getElementById(div).style.display = "block";
-	}
-
+$('document').ready(function(){
+	//Montre/cache les div en fonction des boutons (ou liens de la navbar)
+	$('.caillouShowHide').click(function () {
+		$('.lineBreak25').addClass('displayNone');
+		$($(this).data('target')).removeClass('displayNone');
+	});
+	//Referme la navbar au clic d'un des liens
 	$('.nav-item').click(function(){
 		$('#navbarSupportedContent').removeClass('show');
 	});
-
-	/*$('#jqueryPresentation').click(displayShow('caillouPresentation'));
-
-	function yoloipédique(){
-		$('.TEST').css('color','yellow');
-	}
-
-	$('.TEST').click(yoloipédique());*/
-//});
+});
