0ee9ef95e5328e9081244a81758b6231a20ff7ae
PierreB <floods@hotmail.fr>
2019-01-15 10:35:31 +0100

    Navbar se referme au changement d'ancre

--- Assets/js/cvNum.js
+++ Assets/js/cvNum.js
@@ -1,8 +1,23 @@
-function displayShow(div) {
-    document.getElementById('caillouPresentation').style.display = "none";
-    document.getElementById('caillouCompetences').style.display = "none";
-    document.getElementById('caillouFormation').style.display = "none";
-    document.getElementById('caillouExperiences').style.display = "none";
-    document.getElementById('caillouContact').style.display = "none";
-    document.getElementById(div).style.display = "block";
-}
+//$('document').ready(function(){
+
+	function displayShow(div) {
+	    document.getElementById('caillouPresentation').style.display = "none";
+	    document.getElementById('caillouCompetences').style.display = "none";
+	    document.getElementById('caillouFormation').style.display = "none";
+	    document.getElementById('caillouExperiences').style.display = "none";
+	    document.getElementById('caillouContact').style.display = "none";
+	    document.getElementById(div).style.display = "block";
+	}
+
+	$('.nav-item').click(function(){
+		$('#navbarSupportedContent').removeClass('show');
+	});
+
+	/*$('#jqueryPresentation').click(displayShow('caillouPresentation'));
+
+	function yoloipédique(){
+		$('.TEST').css('color','yellow');
+	}
+
+	$('.TEST').click(yoloipédique());*/
+//});
