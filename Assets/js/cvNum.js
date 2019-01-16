e53716b395c37ec194763b3b13bd7d83c6d72036
PierreB <floods@hotmail.fr>
2019-01-16 14:10:26 +0100

    Retrait bouton inutile, amélioration ancrage des liens en mobile

--- Assets/js/cvNum.js
+++ Assets/js/cvNum.js
@@ -3,9 +3,17 @@ $('document').ready(function(){
 	$('.caillouShowHide').click(function () {
 		$('.lineBreak25').addClass('displayNone');
 		$($(this).data('target')).removeClass('displayNone');
+		//Efface bouton actif
+		if ($(this).hasClass('btn')) {
+			$('.btn').removeClass('displayNone');
+			$($(this).addClass('displayNone'));
+		}
+		//Affiche titre de la div active
+		$('#navbarTitle').text($(this).text());
 	});
 	//Referme la navbar au clic d'un des liens
 	$('.nav-item').click(function(){
 		$('#navbarSupportedContent').removeClass('show');
+		$('html,body').animate({scrollTop: $('#YOUPLABOUM').offset().top}, 'slow');// Euh...?
 	});
 });
